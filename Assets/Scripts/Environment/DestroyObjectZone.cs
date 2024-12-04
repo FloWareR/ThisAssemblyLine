bb299f7fb54a4f036df4ce8d4b10d7faae6603e3
unknown <alphaceves@gmail.com>
2024-12-04 00:55:51 -0600

    XD

--- Assets/Scripts/Environment/DestroyObjectZone.cs
+++ Assets/Scripts/Environment/DestroyObjectZone.cs
@@ -7,6 +7,7 @@ namespace Environment
     {
         private void OnTriggerEnter(Collider other)
         {
+            Debug.Log(other.tag);
             if (other.CompareTag("Player")) return;
             ObjectPoolManager.Instance.ReturnToPool(other.name, other.gameObject);
         }
